912f372b34977e64d0721c577f6d9b643d304cf6
Donatas Bražionis <4donatas4@gmail.com>
2019-09-20 11:42:57 +0300

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace pdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var incomingFileLocation = $"{AppContext.BaseDirectory}data";
+            var text = File.ReadAllText(incomingFileLocation);
+
+            var pdfInBytes = Convert.FromBase64String(text);
+
+            var fileName = DateTimeOffset.Now.ToString("yyyyMMdd'_'HHmmss");
+            var resultFileLocation = $"{AppContext.BaseDirectory}result_{fileName}.pdf";
+            File.WriteAllBytes(resultFileLocation, pdfInBytes);
+
+            Console.WriteLine($"Generated file location: {resultFileLocation}");
+        }
+    }
+}
